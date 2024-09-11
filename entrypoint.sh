45cfee7381c280ebbf83bfc4c5cc18543afc44ae
Ken Blasko <kjb820@gmail.com>
2024-09-11 15:51:31 -0600

    Remove github.workspace dep

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -z "$(echo "$INPUT_REMOTE_PATH" | awk '{$1=$1};1')" ]; then
-    echo "The remote_path can not be empty. see: github.com/Burnett01/rsync-deployments/issues/44"
-    exit 1
+  echo "The remote_path can not be empty. see: github.com/Burnett01/rsync-deployments/issues/44"
+  exit 1
 fi
 
 # Start the SSH agent and load key.
@@ -18,8 +18,7 @@ LEGACY_RSA_HOSTKEYS=$([ "$INPUT_LEGACY_ALLOW_RSA_HOSTKEYS" = "true" ] && echo "$
 
 SWITCHES="$INPUT_SWITCHES"
 RSH="ssh -o StrictHostKeyChecking=no $LEGACY_RSA_HOSTKEYS -p $INPUT_REMOTE_PORT $INPUT_RSH"
-LOCAL_PATH="$GITHUB_WORKSPACE/$INPUT_PATH"
 DSN="$INPUT_REMOTE_USER@$INPUT_REMOTE_HOST"
 
 # Deploy.
-sh -c "rsync $SWITCHES -e '$RSH' $LOCAL_PATH $DSN:$INPUT_REMOTE_PATH"
+sh -c "rsync $SWITCHES -e '$RSH' $INPUT_PATH $DSN:$INPUT_REMOTE_PATH"
